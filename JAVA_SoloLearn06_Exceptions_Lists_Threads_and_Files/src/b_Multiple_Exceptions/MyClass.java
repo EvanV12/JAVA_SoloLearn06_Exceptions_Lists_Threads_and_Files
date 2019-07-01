293a9babe2c6cd68799657ed1c2c3684371e619f
EvanV12 <44060244+EvanV12@users.noreply.github.com>
2019-07-01 19:58:36 +0300

    Added "Multiple Exceptions"

---
+++ JAVA_SoloLearn06_Exceptions_Lists_Threads_and_Files/src/b_Multiple_Exceptions/MyClass.java
@@ -0,0 +1,33 @@
+package b_Multiple_Exceptions;
+
+public class MyClass {
+
+
+	static int div(int a, int b) throws ArithmeticException {
+		if (b == 0) {
+			throw new ArithmeticException("Division by Zero");
+		} else {
+			return a / b;
+		}
+	}
+
+	/*
+	 try {
+  			//some code
+		} catch (ExceptionType1 e1) {
+  			//Catch block
+		} catch (ExceptionType2 e2) {
+  			//Catch block
+		} catch (ExceptionType3 e3) {
+  			//Catch block
+		}
+	 */
+
+
+	public static void main(String[] args) {
+
+		div(5, 0);
+
+	}
+
+}
